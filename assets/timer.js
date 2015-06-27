9011d5f12d9ec85124c644d089982b172ee90422
coinableS <coinables@gmail.com>
2015-06-26 21:33:27 -0700

    Bitcoin Network Health
    Chain websocket API to monitor new network transactions. Measures
    transactions per second and displays percentage of network capacity of
    the 7 txs per second.

---
+++ assets/timer.js
@@ -0,0 +1,8 @@
+var totalTx = 0;
+var totalSeconds = 0;
+        setInterval(setTime, 1000);
+
+        function setTime()
+        {
+            ++totalSeconds;
+        }
---
+++ assets/websocket.js
@@ -0,0 +1,49 @@
+var chainWS = new WebSocket('wss://ws.chain.com/v2/notifications');
+
+chainWS.onopen = function()
+	{
+	chainWS.send(JSON.stringify( 
+        {type: "new-transaction", block_chain: "bitcoin"}
+         ) );
+	};
+
+
+chainWS.onmessage = function(onmsg)
+	{  
+        ++totalTx;
+    var response = JSON.parse(onmsg.data);
+    var amount = response.payload.transaction.amount;
+	var calAmount = amount / 100000000;   
+	$('#stand').html("&#3647;" + calAmount);    
+	$('#UTX').html("<p></p>"); 
+    var txperSec =  totalTx/totalSeconds;   
+    $('#txps').html(txperSec.toFixed(2) + "TX/s");
+    var limit = txperSec/0.07;
+    $('#limit').html(limit.toFixed(0) + "%");    
+    $("p").animate({
+        left: '-200px',
+        });
+
+
+if(limit > 19){
+  	$("#meter1").css("background-color", "green");
+	} else {
+	$("#meter1").css("background-color", "gray");
+	}
+if(limit > 39){
+  	$("#meter2").css("background-color", "yellow");
+	} else {
+	$("#meter2").css("background-color", "gray");
+	}
+if(limit > 59){
+  	$("#meter3").css("background-color", "orange");
+	} else {
+	$("#meter3").css("background-color", "gray");
+	}
+if(limit > 79){
+  	$("#meter4").css("background-color", "red");
+	} else {
+	$("#meter4").css("background-color", "gray");
+	}
+
+}
